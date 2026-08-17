c3f1fc5007e6c4f437c1735fbff18b21d1b371fb
Reketino <bjornevensk8@gmail.com>
2026-08-17 10:23:03 +0200

    feat: added requireAdmin page w/ logic

--- src/lib/auth/requireAdmin.ts
+++ src/lib/auth/requireAdmin.ts
@@ -1,2 +1,29 @@
 import { redirect } from "next/navigation";
 import { createClient } from "../supabase/server";
+
+export async function requireAdmin() {
+    const supabase = await createClient();
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+        redirect("/login");
+    }
+
+    const { data: profile, error} = await supabase
+    .from("profiles")
+    .select("role")
+    .eq("id", user.id)
+    .single()
+
+    if (error || profile?.role !== "admin") {
+        redirect("/");
+    }
+
+    return {
+        user,
+        profile,
+    };
+}
