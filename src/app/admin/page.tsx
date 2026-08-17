bdfb5174fc3dfb6159eda61f1aa04307fea72ffd
Reketino <bjornevensk8@gmail.com>
2026-08-17 10:10:30 +0200

    feat: added admin page log in & moved loginform into auth

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,4 +2,16 @@ import { createClient } from "@/lib/supabase/server";
 
 export default async function AdminPage() {
     const supabase = await createClient()
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    return <main>
+        <h1>Mariudesign Admin</h1>
+
+        <p>
+            Logged in as: {user?.email}
+        </p>
+    </main>
 }
