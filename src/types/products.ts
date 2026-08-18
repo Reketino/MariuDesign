8cb0dbba671ea87b4900e68ec172562d7f5a91d1
Reketino <bjornevensk8@gmail.com>
2026-08-18 11:59:02 +0200

    feat: added export type product

--- src/types/products.ts
+++ src/types/products.ts
@@ -2,4 +2,16 @@ export type ProductCategory = {
     id: string;
     name: string;
     slug: string;
-}
+};
+
+export type Product = {
+    id: string;
+    title: string;
+    slug: string;
+    description: string | null;
+    status: string;
+    license: string | null;
+    created_at: string;
+    category_id: string | null;
+    categories: ProductCategory | ProductCategory | null;
+};
