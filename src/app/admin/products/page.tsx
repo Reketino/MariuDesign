c10cdcf684b35447d3f8cd972c05f6144e972f76
Reketino <bjornevensk8@gmail.com>
2026-08-18 11:36:31 +0200

    feat: added const supabase to initalize supabase client & added code to fetch a list of products from supabase

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -3,5 +3,27 @@ import Link from "next/link";
 import { createClient } from "@/lib/supabase/server";
 
 export default async function ProductsPage() {
+    const supabase = await createClient();
+
+    const { data: products, error } = await supabase
+    .from("products")
+    .select(`
+        id,
+        title,
+        slug,
+        description,
+        status,
+        license,
+        created_at,
+        category_id,
+        categories (
+        id,
+        name,
+        slug
+        )
+        `)
+        .order("created_at", {
+            ascending: false,
+        })
     
 }
