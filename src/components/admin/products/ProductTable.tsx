0147c44b11625f11112a0f349751771945bb743f
Reketino <bjornevensk8@gmail.com>
2026-08-19 10:03:03 +0200

    feat: added function formatDate

--- src/components/admin/products/ProductTable.tsx
+++ src/components/admin/products/ProductTable.tsx
@@ -19,3 +19,11 @@ function getCategory(
 
     return categories;
 }
+
+function formatDate(date: string) {
+    return new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+    }).format(new Date(date));
+}
