207869841e6e4931d560101225a1cceede5269f6
Reketino <bjornevensk8@gmail.com>
2026-08-19 10:07:53 +0200

    feat: added case archived w/ styling

--- src/components/admin/products/ProductTable.tsx
+++ src/components/admin/products/ProductTable.tsx
@@ -32,5 +32,11 @@ function getStatusClasses(status: string) {
     switch(status.toLowerCase()) {
         case "published": 
         return "bg-emerald-500/10 text-emerald-400 ring-1 ring-inset ring-emerald-500/20";
+
+        case "draft":
+            return "bg-zinc-800 text-zinc-400 ring-1 ring-inset ring-zinc-700";
+
+        case "archived":
+            return "bg-red-500/10 text-red-400 ring-1 ring-inset ring-red-500/20";
     }
 }
