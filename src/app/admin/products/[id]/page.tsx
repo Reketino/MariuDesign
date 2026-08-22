23da6918be7416a95b9dbc98e6f2664ced550ceb
Reketino <bjornevensk8@gmail.com>
2026-08-22 10:20:08 +0200

    feat: Added type EditProductPageProps

--- src/app/admin/products/[id]/page.tsx
+++ src/app/admin/products/[id]/page.tsx
@@ -3,3 +3,9 @@ import Link from "next/link";
 import ProductForm from "@/components/admin/products/ProductForm";
 
 import { createClient } from "@/lib/supabase/server";
+
+type EditProductPageProps = {
+    params: Promise<{
+        id: string;
+    }>;
+};
