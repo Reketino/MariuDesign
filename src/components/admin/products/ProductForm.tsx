ee3a6e715f429238d3acddbc7b66ad249626512b
Reketino <bjornevensk8@gmail.com>
2026-08-23 10:16:09 +0200

    feat: added const confirmed w/ text in handleDelete

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -110,6 +110,10 @@ export default function ProductForm({
         if (!product) {
             return;
         }
+
+        const confirmed = window.confirm(
+            `Are you sure Reite you want to delete this "${product.title}"? Be aware this action cannot be undone!.`,
+        );
     }
 
     return (
