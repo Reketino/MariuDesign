4732d7f285b515b85c7ebf76c6f833ce4274c94c
Reketino <bjornevensk8@gmail.com>
2026-08-24 13:13:33 +0200

    feat: setup categoriespage function w/ const supabase & data

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,3 +1,13 @@
 import Link from "next/link";
 
 import { createClient } from "@/lib/supabase/server";
+
+export default async function CategoriesPage() {
+    const supabase = await createClient();
+
+    const { data: categories, error } = await supabase
+        .from("categories")
+        .select("id, name, slug")
+        .order("name");
+    }
+}
