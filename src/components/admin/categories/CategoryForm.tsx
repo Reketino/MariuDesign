ef7d2fa40fb94a4208c05765669c1ae4682b9a68
Reketino <bjornevensk8@gmail.com>
2026-08-25 10:09:18 +0200

    feat: setup & added const router, name, slug & loading

--- src/components/admin/categories/CategoryForm.tsx
+++ src/components/admin/categories/CategoryForm.tsx
@@ -5,3 +5,12 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/client";
+
+export default function CategoryForm() {
+    const router = useRouter();
+
+    const [name, setName] = useState("");
+    const [slug, setSlug] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+}
