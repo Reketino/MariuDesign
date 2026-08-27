361b1b56388abef9b44ba78b7c0da75a38c54b87
Reketino <bjornevensk8@gmail.com>
2026-08-27 10:16:10 +0200

    feat: added const router, loading & error

--- src/components/admin/categories/DeleteCategoryButton.tsx
+++ src/components/admin/categories/DeleteCategoryButton.tsx
@@ -14,4 +14,11 @@ type DeleteCategoryButtonProps = {
 export default function DeleteCategoryButton({
     categoryId,
     categoryName,
-})
+}: DeleteCategoryButtonProps) {
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    
+}
