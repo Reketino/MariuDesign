017b54c981a09baf425f8f1576ce2dffabb7594e
Reketino <bjornevensk8@gmail.com>
2026-08-28 10:22:05 +0200

    feat: added export default function & const router & const loading

--- src/components/admin/products/DeleteProductButton.tsx
+++ src/components/admin/products/DeleteProductButton.tsx
@@ -9,4 +9,21 @@ import { createClient } from "@/lib/supabase/client";
 type DeleteProductButtonProps = {
     productId: string;
     productTitle: string;
+};
+
+export default function DeleteCategoryButton({
+    productId,
+    productTitle,
+}: DeleteProductButtonProps) {
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    async function handleDelete() {
+        const confirmed = window.confirm(
+            `Are you sure you want delete "${productTitle}"?`,
+        );
+    }
 }
+
