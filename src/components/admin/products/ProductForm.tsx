3edd978b83c0648edd26eb27f01e759751bc6467
Reketino <bjornevensk8@gmail.com>
2026-08-30 09:49:24 +0200

    feat: added const allowed image types

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -18,6 +18,12 @@ type ProductFormProps = {
 
 const MAX_IMAGE_SIZE = 5 * 1024 * 1024;
 
+const ALLOWED_IMAGE_TYPES = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+]
+
 export default function ProductForm({
     categories,
     product,
