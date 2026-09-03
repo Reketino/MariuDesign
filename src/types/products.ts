071a3ba78f622a4ba13abbe78ea1b3ae17d746d3
Reketino <bjornevensk8@gmail.com>
2026-09-03 10:13:02 +0200

    feat: added export type productimage w/ id, storage, alt & sort to types

--- src/types/products.ts
+++ src/types/products.ts
@@ -4,6 +4,13 @@ export type ProductCategory = {
   slug: string;
 };
 
+export type ProductImage = {
+  id: string;
+  storage_path: string;
+  alt_text: string | null;
+  sort_order: number;
+}
+
 export type Product = {
   id: string;
   title: string;
