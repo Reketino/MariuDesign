879224034b8f0a4ebc934facc2480052eb54362e
Reketino <bjornevensk8@gmail.com>
2026-09-05 01:25:03 +0200

    style: fix indentation in ProductImages component

--- src/components/admin/products/ProductImage.tsx
+++ src/components/admin/products/ProductImage.tsx
@@ -32,7 +32,7 @@ export default function ProductImages({
                 </p>
             </header>
 
-             {existingImage && !image && (
+            {existingImage && !image && (
                 <div className="mt-6">
                     <p className="mb-3 text-sm font-medium text-zinc-300">
                         Current image
