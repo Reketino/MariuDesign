4acbcb61f148a482d715bb8ad4c2c50ebed41ec7
Reketino <bjornevensk8@gmail.com>
2026-09-04 10:09:21 +0200

    feat: added function to getProductImageUrl

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -44,6 +44,10 @@ function getProductImage(
     );
 }
 
+function getProductImageUrl(storagePath: string) {
+    return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/product-images/${storagePath}`;
+}
+
 export default function ProductForm({
     categories,
     product,
