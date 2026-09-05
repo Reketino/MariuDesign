12f5d6f70c04ea5e166e83dd3370cbf8c490c3b2
Reketino <bjornevensk8@gmail.com>
2026-09-05 10:47:59 +0200

    feat: added return & export of productImage function to get product image from product object

--- src/components/admin/products/utils/productImage.ts
+++ src/components/admin/products/utils/productImage.ts
@@ -1,1 +1,9 @@
 import type { ProductImage } from "@/types/products";
+
+export function getProductImage(
+    images: ProductImage[] | null,
+): ProductImage | null {
+    if (!images) {
+        return null;
+    }
+}
