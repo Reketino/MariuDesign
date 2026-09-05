e125013c9235eeb7495b96a234f69e779e7a53ef
Reketino <bjornevensk8@gmail.com>
2026-09-05 10:23:13 +0200

    chore: removed supabase from uploadProductImage function

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -117,10 +117,7 @@ export default function ProductForm({
         setImage(selectedFile);
     }
 
-    async function uploadProductImage(
-        supabase: ReturnType<typeof createClient>,
-        productId: string,
-    ) {
+    async function uploadProductImage(productId: string) {
         if (!image) {
             return;
         }
