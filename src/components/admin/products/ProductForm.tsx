d77a33e3b458b0b09eb6f1920f86f079b6513b4b
Reketino <bjornevensk8@gmail.com>
2026-09-08 10:04:42 +0200

    style: formatting, no logic changes

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -179,31 +179,31 @@ export default function ProductForm({
     }
 
     async function handleSetMainImage(image: ProductImage) {
-    if (!product) {
-        return;
+        if (!product) {
+            return;
+        }
+
+        setError("");
+        setLoading(true);
+
+        try {
+            await setProductImageAsMain({
+                supabase,
+                productId: product.id,
+                imageId: image.id,
+            });
+
+            router.refresh();
+        } catch (error) {
+            setError(
+                error instanceof Error
+                    ? error.message
+                    : "Something went wrong while setting the main product image.",
+            );
+        } finally {
+            setLoading(false);
+        }
     }
-
-    setError("");
-    setLoading(true);
-
-    try {
-        await setProductImageAsMain({
-            supabase,
-            productId: product.id,
-            imageId: image.id,
-        });
-
-        router.refresh();
-    } catch (error) {
-        setError(
-            error instanceof Error
-                ? error.message
-                : "Something went wrong while setting the main product image.",
-        );
-    } finally {
-        setLoading(false);
-    }
-}
 
     async function handleDelete() {
         if (!product) {
