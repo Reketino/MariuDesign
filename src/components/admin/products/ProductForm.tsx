698efcbdb7f69fa72967b1b943dad6af60027c81
Reketino <bjornevensk8@gmail.com>
2026-09-08 10:05:13 +0200

    feat: added onSetMainImage to ProductImages in product form

--- src/components/admin/products/ProductForm.tsx
+++ src/components/admin/products/ProductForm.tsx
@@ -272,6 +272,7 @@ export default function ProductForm({
                 }))}
                 onImageChange={handleImageChange}
                 onDeleteImage={handleDeleteImage}
+                onSetMainImage={handleSetMainImage}
             />
 
             {error && (
