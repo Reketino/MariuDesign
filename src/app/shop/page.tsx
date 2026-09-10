e2507629d9496ccac347297e24d1bc9006674024
Reketino <bjornevensk8@gmail.com>
2026-09-10 10:12:17 +0200

    feat: added type shop product w/ title, id, slug & categories

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -6,3 +6,15 @@ import { getSupabaseServerClient } from "@/lib/supabase";
 import type { ProductImage } from "@/types/products";
 
 import { getProductImageUrl } from "@/components/admin/products/utils/productImage";
+
+type ShopProduct = {
+    id: string;
+    title: string;
+    slug: string;
+    categories: {
+        id: string;
+        name: string;
+        slug: string;
+    }[] | null;
+    products_images: ProductImage[] | null;
+};
