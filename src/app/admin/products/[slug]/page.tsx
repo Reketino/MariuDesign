585a7c52048aab17d683a5f9a303c1843bf9dce7
Reketino <bjornevensk8@gmail.com>
2026-09-10 10:27:50 +0200

    feat: added type ProductPageData in slug page.tsx

--- src/app/admin/products/[slug]/page.tsx
+++ src/app/admin/products/[slug]/page.tsx
@@ -7,3 +7,17 @@ import { createClient } from "@/lib/supabase/server";
 import type { ProductImage } from "@/types/products";
 
 import { getProductImageUrl } from "@/components/admin/products/utils/productImage";
+
+type ProductPageData = {
+    id: string;
+    title: string;
+    slug: string;
+    description: string | null;
+    license: string | null;
+    categories: {
+        id: string;
+        name: string;
+        slug: string;
+    }[] | null;
+    product_images: ProductImage[] | null;
+}
