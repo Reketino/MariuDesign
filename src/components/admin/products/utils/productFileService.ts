b581c415ed3918426c26174ffe7dfe7b3af4a0f8
Reketino <bjornevensk8@gmail.com>
2026-09-11 09:55:10 +0200

    feat: added type uploadproductfileparams w/ supabase, productid, file & version

--- src/components/admin/products/utils/productFileService.ts
+++ src/components/admin/products/utils/productFileService.ts
@@ -1,1 +1,8 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
+
+type UploadProductFileParams = {
+    supabase: SupabaseClient;
+    productId: string;
+    file: File;
+    version: string;
+}
