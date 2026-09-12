2889afa50e6f37499d8e48677e4cfd7f9f2c6850
Reketino <bjornevensk8@gmail.com>
2026-09-13 00:02:52 +0200

    feat: define ProductFilesProps type for ProductFiles component

--- src/components/admin/products/ProductFiles.tsx
+++ src/components/admin/products/ProductFiles.tsx
@@ -2,3 +2,13 @@
 
 import { useState } from "react";
 
+import type { ProductFile } from "@/types/products";
+
+type ProductFilesProps = {
+    existingFiles: ProductFile[];
+    selectedFiles: File[];
+    version: string;
+    onFilesChange: (files: File[]) => void;
+    onVersionChange: (version: string) => void;
+    onDeleteExistingFile: (file: ProductFile) => Promise<void>;
+};
