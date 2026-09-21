e39729235e445e8f057cbc579464031458cc0c25
Reketino <bjornevensk8@gmail.com>
2026-09-21 10:17:04 +0200

    feat: added type routecontext w/ params & slug

--- src/app/api/[slug]/download/route.ts
+++ src/app/api/[slug]/download/route.ts
@@ -1,3 +1,9 @@
 import { NextResponse } from "next/server";
 
 import { createClient } from "@/lib/supabase/server";
+
+type RouteContext = {
+    params: Promise<{
+        slug: string;
+    }>;
+};
