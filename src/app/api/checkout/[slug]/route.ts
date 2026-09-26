651f9e53d41004481c7f089af1eef3ae3a628721
Reketino <bjornevensk8@gmail.com>
2026-09-26 10:07:38 +0200

    feat: new type routecontext w/ params & slug

--- src/app/api/checkout/[slug]/route.ts
+++ src/app/api/checkout/[slug]/route.ts
@@ -2,3 +2,9 @@ import { NextResponse } from "next/server";
 
 import { stripe } from "@/lib/stripe/server";
 import { createClient } from "@/lib/supabase/server";
+
+type RouteContext = {
+    params: Promise<{
+        slug: string;
+    }>;
+}
