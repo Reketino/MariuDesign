a67ea708f9dd3ec1320dc7c3935bd0cc4d1e6457
Reketino <bjornevensk8@gmail.com>
2026-09-26 10:12:34 +0200

    feat: added const data w/ await supabase to getuser

--- src/app/api/checkout/[slug]/route.ts
+++ src/app/api/checkout/[slug]/route.ts
@@ -16,5 +16,9 @@ export async function POST(
     const { slug } = await params;
 
     const supabase = await createClient();
-    
+
+    const { data: {
+        user,
+    },
+    } = await supabase.auth.getUser();
 }
